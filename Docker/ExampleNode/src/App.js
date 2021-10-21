d45ab1f29b79575ecb728406e390cc4eed732768
anasthasia_manu <anasthasia.manu@quantumblack.com>
2021-10-21 16:07:40 +0100

    Changed the work feature branch

--- Docker/ExampleNode/src/App.js
+++ Docker/ExampleNode/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye bumbum There!!</p>
+        <p>I was changed on the feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
